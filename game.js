d0f58c6a61f03c9ae417507792d0effc9c0cf9de
Monica Cedeno <monica.cedeno@gmail.com>
2022-12-08 14:13:06 -0500

    cleaned up logic and added more tests

--- game.js
+++ game.js
@@ -1,121 +1,120 @@
 export class TykTacToe {
-  topLeft = [-1, -1];
-  top = [-1, 0];
-  topRight = [-1, 1];
-  right = [0, 1];
-  botRight = [1, 1];
-  bot = [1, 0];
-  botLeft = [1, -1];
-  left = [0, -1];
-  directions = [this.topLeft, this.top, this.topRight, this.right];
-  straights = new Map([
-    [this.topLeft, this.botRight],
-    [this.top, this.bot],
-    [this.topRight, this.botLeft],
-    [this.right, this.left]
-  ]);
-  lastPlacement;
+    topLeft = [-1, -1];
+    top = [-1, 0];
+    topRight = [-1, 1];
+    right = [0, 1];
+    botRight = [1, 1];
+    bot = [1, 0];
+    botLeft = [1, -1];
+    left = [0, -1];
+    directions = [this.topLeft, this.top, this.topRight, this.right];
+    oppositeDirections = new Map([
+        [this.topLeft, this.botRight],
+        [this.top, this.bot],
+        [this.topRight, this.botLeft],
+        [this.right, this.left]
+    ]);
+    lastPlacement;
 
-  constructor(length, grid) {
-    // the grid is always square
-    this.gridLength = length;
-    if (grid) {
-      this.grid = grid;
-    } else {
-      this.grid = this.generateGrid();
-    }
-  }
-
-  generateGrid() {
-    const grid = [];
-    for (let i = 0; i < this.gridLength; i++) {
-      grid.push([]);
-      for (let j = 0; j < this.gridLength; j++) {
-        grid[i].push('-');
-      }
-    }
-    return grid;
-  }
-
-  isNullOrUndefined(query) {
-    return query === null || query === undefined;
-  }
-
-  areCoordinatesValid(coordinates) {
-    return !(this.isNullOrUndefined(coordinates) || coordinates.length !== 2);
-  }
-
-  placeToken(coordinates, token) {
-    if (!this.areCoordinatesValid(coordinates) || !this.areCoordinatesInBounds(coordinates)) {
-      return;
-    }
-    // coordinates is an array of 2 elements
-    // we need to validate that in a new test
-    //if coordinate is already a token then reject
-    if (!this.doesTileContainToken(coordinates, '-')) { // only '-' is a valid tile to place a token
-      return;
-    }
-    // indices of arrays return a copy of the value
-    this.grid[coordinates[0]][coordinates[1]] = token;
-
-    this.lastPlacement = coordinates;
-  }
-
-  areCoordinatesInBounds(coordinates) {
-    return coordinates[0] >= 0 && coordinates[0] < this.gridLength
-      && coordinates[1] >= 0 && coordinates[1] < this.gridLength;
-  }
-
-  getLastPlacement() {
-    return this.lastPlacement;
-  }
-
-  getGrid() {
-    return this.grid;
-  }
-
-  addCoordinates(first, second) {
-    const newY = first[0] + second[0];
-    const newX = first[1] + second[1];
-    return [newY, newX];
-  }
-
-  doesTileContainToken(tileToCheck, token) {
-    return this.grid[tileToCheck[0]][tileToCheck[1]] === token;
-  }
-
-  doesDirectionHaveWinner(direction, originalTile, token) {
-    // we test the direction or the opposite of that direction in sequence
-    const possibleDirections = [direction, this.straights.get(direction)];
-    const requiredTokens = this.gridLength - 1; // a square grid; minus one because we already have the original token
-    let successiveTokens = 0; // if this matches the requiredTokens, the last player made a winning move
-    let currentTile;
-    for (const possibleDirection of possibleDirections) {
-      currentTile = originalTile;
-      while (successiveTokens < requiredTokens) {
-        const nextTile = this.addCoordinates(possibleDirection, currentTile);
-        if (!this.areCoordinatesInBounds(nextTile)) {
-          break;
+    constructor(length, grid) {
+        // the grid is always square
+        this.gridLength = length;
+        if (Array.isArray(grid) && grid.length > 0 && grid.length === grid[0].length) {
+            this.grid = grid;
+        } else {
+            this.grid = this.generateGrid();
         }
-        if (!this.doesTileContainToken(nextTile, token)) {
-          return false;
-        }
-        successiveTokens += 1;
-        currentTile = nextTile;
-      }
     }
-    return successiveTokens === requiredTokens;
-  }
 
-  hasLastPlayerWon() {
-    const oldY = this.lastPlacement[0];
-    const oldX = this.lastPlacement[1];
-    const token = this.grid[oldY][oldX];
-    for (const direction of this.directions) {
-      if (this.doesDirectionHaveWinner(direction, [oldY, oldX], token)) {
-        return true;
-      }
+    generateGrid() {
+        const grid = [];
+        for (let i = 0; i < this.gridLength; i++) {
+            grid.push([]);
+            for (let j = 0; j < this.gridLength; j++) {
+                grid[i].push('-');
+            }
+        }
+        return grid;
     }
-  return false;
-  }
+
+    isNullOrUndefined(query) {
+        return query === null || query === undefined;
+    }
+
+    areCoordinatesValid(coordinates) {
+        return !(this.isNullOrUndefined(coordinates) || coordinates.length !== 2);
+    }
+
+    placeToken(coordinates, token) {
+        if (!this.areCoordinatesValid(coordinates) || !this.areCoordinatesInBounds(coordinates)) {
+            return;
+        }
+        // coordinates is an array of 2 elements
+        // if coordinate is already a token then reject
+        if (!this.doesTileContainToken(coordinates, '-')) { // only '-' is a valid tile to place a token
+            return;
+        }
+
+        this.grid[coordinates[0]][coordinates[1]] = token;
+
+        this.lastPlacement = coordinates;
+    }
+
+    areCoordinatesInBounds(coordinates) {
+        return coordinates[0] >= 0 && coordinates[0] < this.gridLength
+            && coordinates[1] >= 0 && coordinates[1] < this.gridLength;
+    }
+
+    getLastPlacement() {
+        return this.lastPlacement;
+    }
+
+    getGrid() {
+        return this.grid;
+    }
+
+    addCoordinates(first, second) {
+        const newY = first[0] + second[0];
+        const newX = first[1] + second[1];
+        return [newY, newX];
+    }
+
+    doesTileContainToken(tileToCheck, token) {
+        return this.grid[tileToCheck[0]][tileToCheck[1]] === token;
+    }
+
+    doesDirectionHaveWinner(direction, token) {
+        // we test the direction or the opposite of that direction in sequence
+        const possibleDirections = [direction, this.oppositeDirections.get(direction)];
+        const requiredTokens = this.gridLength - 1; // a square grid; minus one because we already have the original token
+        let successiveTokens = 0; // if this matches the requiredTokens, the last player made a winning move
+        let currentTile;
+        for (const possibleDirection of possibleDirections) {
+            currentTile = this.lastPlacement;
+            while (successiveTokens < requiredTokens) {
+                const nextTile = this.addCoordinates(possibleDirection, currentTile);
+                if (!this.areCoordinatesInBounds(nextTile)) {
+                    break;
+                }
+                if (!this.doesTileContainToken(nextTile, token)) {
+                    return false;
+                }
+                successiveTokens += 1;
+                currentTile = nextTile;
+            }
+        }
+        return successiveTokens === requiredTokens;
+    }
+
+    hasLastPlayerWon() {
+        const oldY = this.lastPlacement[0];
+        const oldX = this.lastPlacement[1];
+        const token = this.grid[oldY][oldX];
+        for (const direction of this.directions) {
+            if (this.doesDirectionHaveWinner(direction, token)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- tests/game.test.js
+++ tests/game.test.js
@@ -3,48 +3,69 @@ import {TykTacToe} from "../game";
 describe('game tests', () => {
     it('creates a square grid', () => {
         const t = new TykTacToe(3, null);
-        expect(t.generateGrid()).toEqual([['-','-','-'],['-','-','-'],['-','-','-']]);
+        expect(t.generateGrid()).toEqual([['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]);
         expect(t.getGrid().length === t.getGrid()[0].length);
+    });
+
+    it('rejects a non-square grid', () => {
+        const grid = [['-', '-', '-']];
+        const t = new TykTacToe(4, grid);
+        expect(t.getGrid()).toEqual([['-', '-', '-', '-'], ['-', '-', '-', '-'], ['-', '-', '-', '-'], ['-', '-', '-', '-']]);
+    });
+
+    it('rejects a grid with no columns', () => {
+        const t = new TykTacToe(4, [[]]);
+        expect(t.getGrid()).toEqual([['-', '-', '-', '-'], ['-', '-', '-', '-'], ['-', '-', '-', '-'], ['-', '-', '-', '-']]);
+    });
+
+    it('rejects a grid with no rows', () => {
+        const t = new TykTacToe(4, []);
+        expect(t.getGrid()).toEqual([['-', '-', '-', '-'], ['-', '-', '-', '-'], ['-', '-', '-', '-'], ['-', '-', '-', '-']]);
+    });
+
+    it('rejects a grid with a non-array rows', () => {
+        const t = new TykTacToe(4, [35]);
+        expect(t.getGrid()).toEqual([['-', '-', '-', '-'], ['-', '-', '-', '-'], ['-', '-', '-', '-'], ['-', '-', '-', '-']]);
     });
 
     it('changes the middle tile of the grid to a token', () => {
         const t = new TykTacToe(3, null);
-        t.placeToken([1,1], 'x');
+        t.placeToken([1, 1], 'x');
         expect(t.getGrid()[1][1]).toEqual('x');
     });
 
     it('changes the top right tile of the grid to a token', () => {
         const t = new TykTacToe(3, null);
-        t.placeToken([0,2],'x');
-        expect(t.getGrid()).toEqual([['-','-','x'],['-','-','-'],['-','-','-']]);
+        t.placeToken([0, 2], 'x');
+        expect(t.getGrid()).toEqual([['-', '-', 'x'], ['-', '-', '-'], ['-', '-', '-']]);
     });
 
     it('changes the bottom left tile of the grid to a O', () => {
         const t = new TykTacToe(3, null);
-        t.placeToken([2,0], 'o');
-        expect(t.getGrid()).toEqual([['-','-','-'],['-','-','-'],['o','-','-']]);
+        t.placeToken([2, 0], 'o');
+        expect(t.getGrid()).toEqual([['-', '-', '-'], ['-', '-', '-'], ['o', '-', '-']]);
     });
 
     it('handles out of range coordinates gracefully', () => {
         const t = new TykTacToe(3, null);
-        let isNotInBounds = t.areCoordinatesInBounds([3,0]);
+        let isNotInBounds = t.areCoordinatesInBounds([3, 0]);
         expect(isNotInBounds).toBe(false);
         isNotInBounds = t.areCoordinatesInBounds([0, -1]);
         expect(isNotInBounds).toBe(false);
     });
 
     it('rejects overwriting x token', () => {
-        const grid = [['-','-','-'],['-','x','-'],['-','-','-']];
+        const grid = [['-', '-', '-'], ['-', 'x', '-'], ['-', '-', '-']];
         const t = new TykTacToe(3, grid);
-        t.placeToken([1,1], 'o');
-        expect(t.getGrid()).toEqual([['-','-','-'],['-','x','-'],['-','-','-']]);
+        t.placeToken([1, 1], 'o');
+        expect(t.getGrid()).toEqual([['-', '-', '-'], ['-', 'x', '-'], ['-', '-', '-']]);
     });
 
     it('rejects overwriting o token', () => {
-        const grid = [['-','-','-'],['-','o','-'],['-','-','-']];
+        const grid = [['-', '-', '-'], ['-', 'o', '-'], ['-', '-', '-']];
         const t = new TykTacToe(3, grid);
-        t.placeToken([1,1], 'x');
-        expect(t.getGrid()).toEqual([['-','-','-'],['-','o','-'],['-','-','-']]);
+        t.placeToken([1, 1], 'x');
+        expect(t.getGrid()).toEqual([['-', '-', '-'], ['-', 'o', '-'], ['-', '-', '-']]);
     });
 
     it('rejects coordinates that are not length 2', () => {
@@ -75,13 +96,13 @@ describe('game tests', () => {
     });
 
     it('recognises a tile contains a token #1', () => {
-        const grid = [['-','-','-'],['-','-','x'],['-','-','-']];
+        const grid = [['-', '-', '-'], ['-', '-', 'x'], ['-', '-', '-']];
         const t = new TykTacToe(3, grid);
         expect(t.doesTileContainToken([1, 2], 'x')).toBe(true);
     });
 
     it('recognises a tile contains a token #2', () => {
-        const grid = [['-','-','-'],['-','-','-'],['-','o','-']];
+        const grid = [['-', '-', '-'], ['-', '-', '-'], ['-', 'o', '-']];
         const t = new TykTacToe(3, grid);
         expect(t.doesTileContainToken([2, 1], 'o')).toBe(true);
     });
@@ -102,88 +123,91 @@ describe('game tests', () => {
     });
 
     it('recognises a token has won #1', () => {
-        const grid = [['x','x','x'],['-','-','-'],['-','-','-']];
+        const grid = [['x', '-', 'x'], ['-', '-', '-'], ['-', '-', '-']];
         const t = new TykTacToe(3, grid);
-        expect(t.doesDirectionHaveWinner(t.right, [0, 1], 'x')).toBe(true);
+        t.placeToken([0, 1], 'x');
+        expect(t.doesDirectionHaveWinner(t.right, 'x')).toBe(true);
     });
 
     it('recognises a token has won #2', () => {
-        const grid = [['-','-','o'],['-','o','-'],['o','-','-']];
+        const grid = [['-', '-', 'o'], ['-', '-', '-'], ['o', '-', '-']];
         const t = new TykTacToe(3, grid);
-        expect(t.doesDirectionHaveWinner(t.topRight, [1, 1], 'o')).toBe(true);
+        t.placeToken([1, 1], 'o');
+        expect(t.doesDirectionHaveWinner(t.topRight, 'o')).toBe(true);
     });
 
     it('recognises a token has won #3', () => {
-        const grid = [['-','-','-'],['-','-','-'],['o','o','o']];
+        const grid = [['-', '-', '-'], ['-', '-', '-'], ['o', 'o', '-']];
         const t = new TykTacToe(3, grid);
-        expect(t.doesDirectionHaveWinner(t.right, [2, 2], 'o')).toBe(true);
+        t.placeToken( [2, 2], 'o');
+        expect(t.doesDirectionHaveWinner(t.right, 'o')).toBe(true);
     });
 
     it('recognises someone has won top horizontally', () => {
-        const grid = [['x','x','-'],['-','-','-'],['-','-','-']];
+        const grid = [['x', 'x', '-'], ['-', '-', '-'], ['-', '-', '-']];
         const t = new TykTacToe(3, grid);
-        t.placeToken([0,2], 'x');
+        t.placeToken([0, 2], 'x');
         expect(t.hasLastPlayerWon()).toEqual(true);
     });
 
     it('recognises someone has won middle horizontally', () => {
-        const grid = [['-','-','-'],['x','-','x'],['-','-','-']];
+        const grid = [['-', '-', '-'], ['x', '-', 'x'], ['-', '-', '-']];
         const t = new TykTacToe(3, grid);
         t.placeToken([1, 1], 'x');
         expect(t.hasLastPlayerWon()).toEqual(true);
     });
 
     it('recognises someone has won bottom horizontally', () => {
-        const grid = [['-','-','-'],['-','-','-'],['-','o','o']];
+        const grid = [['-', '-', '-'], ['-', '-', '-'], ['-', 'o', 'o']];
         const t = new TykTacToe(3, grid);
-        t.placeToken([2,0], 'o');
+        t.placeToken([2, 0], 'o');
         expect(t.hasLastPlayerWon()).toEqual(true);
     });
 
     it('recognises someone has won vertically top left', () => {
-        const grid = [['-','-',''],['o','-','-'],['o','','-']];
+        const grid = [['-', '-', ''], ['o', '-', '-'], ['o', '', '-']];
         const t = new TykTacToe(3, grid);
-        t.placeToken([0,0], 'o');
+        t.placeToken([0, 0], 'o');
         expect(t.hasLastPlayerWon()).toEqual(true);
     });
 
     it('recognises someone has won vertically top', () => {
-        const grid = [['-','-',''],['-','o','-'],['-','o','-']];
+        const grid = [['-', '-', ''], ['-', 'o', '-'], ['-', 'o', '-']];
         const t = new TykTacToe(3, grid);
-        t.placeToken([0,1], 'o');
+        t.placeToken([0, 1], 'o');
         expect(t.hasLastPlayerWon()).toEqual(true);
     });
 
     it('recognises someone has won vertically top right', () => {
-        const grid = [['-','-','x'],['-','-','x'],['-','-','-']];
+        const grid = [['-', '-', 'x'], ['-', '-', 'x'], ['-', '-', '-']];
         const t = new TykTacToe(3, grid);
         t.placeToken([2, 2], 'x');
         expect(t.hasLastPlayerWon()).toEqual(true);
     });
 
     it('recognises someone has won diagonally top left', () => {
-        const grid = [['-','-','x'],['-','-','-'],['x','-','-']];
+        const grid = [['-', '-', 'x'], ['-', '-', '-'], ['x', '-', '-']];
         const t = new TykTacToe(3, grid);
         t.placeToken([1, 1], 'x');
         expect(t.hasLastPlayerWon()).toEqual(true);
     });
 
     it('recognises someone has won diagonally top right', () => {
-        const grid = [['x','-','-'],['-','x','-'],['-','-','-']];
+        const grid = [['x', '-', '-'], ['-', 'x', '-'], ['-', '-', '-']];
         const t = new TykTacToe(3, grid);
         t.placeToken([2, 2], 'x');
         expect(t.hasLastPlayerWon()).toEqual(true);
     });
 
     it('recognises no one has won yet #1', () => {
-        const grid = [['','-','-'],['x','x','-'],['-','-','-']];
+        const grid = [['', '-', '-'], ['x', 'x', '-'], ['-', '-', '-']];
         const t = new TykTacToe(3, grid);
         t.placeToken([2, 0], 'x');
         expect(t.hasLastPlayerWon()).toEqual(false);
     });
 
     it('recognises no one has won yet #2', () => {
-        const grid = [['x','x','o'],['o','-','x'],['-','-','o']];
+        const grid = [['x', 'x', 'o'], ['o', '-', 'x'], ['-', '-', 'o']];
         const t = new TykTacToe(3, grid);
         t.placeToken([1, 1], 'x');
         expect(t.hasLastPlayerWon()).toEqual(false);
